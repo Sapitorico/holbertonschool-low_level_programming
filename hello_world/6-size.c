10bee866d301ec66453f27128c0a0cf6add211c4
Sapitorico <renzoyaques@gmail.com>
2022-06-07 16:20:08 -0700

    wdqwdqwd

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main()
+{
+	int a;
+	float b;
+	double c;
+	char d;
+
+printf("Size of int: %zu bytes\n", sizeof(a));
+printf("Size of float: %zu bytes\n", sizeof(b));
+printf("Size of double: %zu bytes\n", sizeof(c));
+printf("Size of char: %zu byte\n", sizeof(d));                    
+	return (0);
+}
