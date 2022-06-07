1f95cb5f7016629d70aa5889d853d17e236c793a
Sapitorico <renzoyaques@gmail.com>
2022-06-07 16:33:52 -0700

    qwsqwdw

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of a char: %zu byte(s)\n", sizeof(a));
 printf("Size of an int: %zu byte(s)\n", sizeof(b));
 printf("Size of a long int: %zu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-printf("Size of a float: %zu byte(s)\n", sizeof(e));                    
+printf("Size of a float: %zu byte(s)\n", sizeof(e));
 	return (0);
 }
