99cdcfa0248979d945e1534ace58751ac6e0b7b8
Sapitorico <renzoyaques@gmail.com>
2022-06-08 04:43:44 -0700

    qwdqwdqw

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,10 +5,11 @@
  *  *  * main - Entry point
  *   *   *
  *    *    * Return: Always 0 (Success)
- *     */
+ */
 int main(void)
 {
 	char low;
+
 	for (low = 'a' ; low <= 'z' ; low++)
 	putchar(low);
 	printf("\n");
