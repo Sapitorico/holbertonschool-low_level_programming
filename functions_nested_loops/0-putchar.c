41ef11cd6b6f4734b815d62a9f9eeaae34557249
Sapitorico <renzoyaques@gmail.com>
2022-06-11 10:20:35 -0700

    qwdwq

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "_putchar.h"
+
+/**
+ *  * main - entry point
+ *   *
+ *    * Description: Prints _putchar
+ *     *
+ *      * Return: always 0
+ */
+int main(void)
+{
+		int i = 0;
+		char c;
+		char s[] = "_putchar\n";
+
+		while (i <= 9)
+{
+		c = s[i];
+		_putchar(c);
+		i++;
+}
+		return (0);
+}
