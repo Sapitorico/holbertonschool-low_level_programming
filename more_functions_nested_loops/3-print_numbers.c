6c5266c16062f2d30010fe2ba42d1f6896f73a62
Sapitorico <renzoyaques@gmail.com>
2022-06-12 11:08:31 -0700

    xw

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_number - prints 0..9
+ *
+ * _putchar - pint c
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+	
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
