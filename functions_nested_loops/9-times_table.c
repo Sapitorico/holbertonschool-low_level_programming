efdd2494f97053cf16e042d2c7e2508d1c0f19a7
Sapitorico <renzoyaques@gmail.com>
2022-06-12 08:22:55 -0700

    xw

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
+ *  * times_table - Print the 9 times table, starting with 0
+ *   * Return: 0
  */
 void times_table(void)
 {
