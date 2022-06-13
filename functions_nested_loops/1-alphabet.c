bb98b0547b2c2e00472db601e1056ee658760fff
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2022-06-13 10:12:57 -0300

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  * print_alphabet - entry point
- *   *
- *    * Description: Prints the alphabet with _putchar
- *     *
- *      * Return: void
+ *print_alphabet - entry point
+ * 
+ *Description: Prints the alphabet with _putchar
+ *
+ *Return: void
  */
 
 void print_alphabet(void)
