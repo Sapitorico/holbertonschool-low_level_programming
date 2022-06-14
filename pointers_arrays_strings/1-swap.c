75569d3f4e9391150e890b82d91144d3ca8d7b70
Sapitorico <renzoyaques@gmail.com>
2022-06-14 11:23:34 -0700

    fix betty style

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/*
- * swap_int - function that swaps the values of two integers
+/**
+ *swap_int - function that swaps the values of two integers
  *
- * @a: pointer to a value
- * @b: pointer to a value
- * @swap_int: variable pinter to a
+ *@a: pointer to a value
+ *@b: pointer to a value
+ *
  */
 void swap_int(int *a, int *b)
 {
