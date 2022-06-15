8e6f34cee88c887837697ca724dcc92fc8d868a8
Sapitorico <renzoyaques@gmail.com>
2022-06-15 12:29:06 -0700

    reverses a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *rev_string - funtion that reverses a string
+ *
+ *@s:pointer to the array str
+ */
+void rev_string(char *s)
+{
+	int i, rp;
+	char ap;
+
+	rp = 0;
+	for (i =0; s[i] !='\0'; i++)
+		rp++;
+
+	for (i = 0; i < rp; i++)
+	{
+		ap = s[i];
+		s[i] = s[rp - 1];
+		s[rp -1] = ap;
+		rp--;
+	}
+}
