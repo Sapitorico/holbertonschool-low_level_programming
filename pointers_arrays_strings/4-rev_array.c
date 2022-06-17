178618bd48df7eba042ad196908d730ffc4742dc
Sapitorico <renzoyaques@gmail.com>
2022-06-17 14:16:00 -0700

    everses the content of an array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverse the order of elements in an array
+ *
+ * @a:wqd
+ * @n:wqd
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
