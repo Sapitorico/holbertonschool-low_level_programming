13b6c6ac946efed98858e60751df6a8be30ce2c4
Sapitorico <renzoyaques@gmail.com>
2022-06-17 23:10:40 -0700

    ocates a character in a string.

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr -  function that locates a character in a string.
+ *
+ * @s:qwd
+ * @c:dwq
+ * Return: s + i
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	if (s[i] == c)
+		return (s);
+	return (0);
+}
