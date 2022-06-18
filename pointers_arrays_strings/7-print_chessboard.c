3101040c69380ca696b1cb9d02d86a94797af61e
Sapitorico <renzoyaques@gmail.com>
2022-06-18 14:07:10 -0700

    prints the chessboard.

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_chessboard - unction that prints the chessboard.
+ *
+ *@a:ewd
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	i = n = 0;
+	while (i < 8)
+	{
+		n = 0;
+		while (n < 8)
+		{
+			_putchar(a[i][n]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
