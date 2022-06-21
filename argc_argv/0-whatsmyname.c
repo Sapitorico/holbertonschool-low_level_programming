b19a71b87411de5c4073449a17686edf242a4069
Sapitorico <renzoyaques@gmail.com>
2022-06-21 16:28:13 -0700

    hat prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - rogram that prints its name
+ * @argc:var_type int
+ * @argv:var_type_ char*
+ * Return:always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+	printf("%s\n", argv[i]);
+	i++;
+	}
+	putchar(10);
+	return (0);
+}
