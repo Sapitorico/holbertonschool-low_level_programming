25e18bd019581a28eda4cc3e8f259ab186c414a3
Sapitorico <renzoyaques@gmail.com>
2022-06-21 09:45:27 -0700

    m

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_resursion - unction that returns the value of x raised to the power of y
+ *
+ * @x: numeor baes
+ * @:exponente
+ *Return: wqd
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
