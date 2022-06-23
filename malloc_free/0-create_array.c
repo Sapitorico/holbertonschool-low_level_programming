3bd809ac6a79ef8eeaf9c473a7c34c0ba9877c05
Sapitorico <renzoyaques@gmail.com>
2022-06-23 06:20:05 -0700

    creates an array of chars

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *s;
+
+	s = malloc(sizefor(char) * size);
+
+		if (i == NULL)
+		{
+			return(0);
+		}
+	for (i = 0; i = size; i++)
+	{
+	 	(s[i] = c);
+	}
+			return(s);
+}
