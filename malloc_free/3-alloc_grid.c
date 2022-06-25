0eadfbc39f02e138186a35c8c5cc543402b1cf76
Sapitorico <renzoyaques@gmail.com>
2022-06-25 12:10:33 -0700

    wq

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,7 +9,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	s = malloc(sizeof(int) * height);
+	s = malloc(sizeof(int *) * height);
 
 	if (s == 0)
 	{
