9a7f57d84619954e52de1a6377e24cc609cf4e06
Sapitorico <renzoyaques@gmail.com>
2022-06-25 15:06:40 -0700

    More malloc, free

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: int of amount of needed memory
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *i;
+
+	i = malloc(b);
+	if (i == NULL)
+	{
+		exit(98);
+	}
+	return(i);
+}
---
+++ more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+
+void *malloc_checked(unsigned int b);
+
+#endif/*main_h*/
