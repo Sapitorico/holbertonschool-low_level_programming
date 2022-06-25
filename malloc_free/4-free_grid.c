7c77d977b841eabfc011352a51f5a981ccb1f2ab
Sapitorico <renzoyaques@gmail.com>
2022-06-25 13:02:16 -0700

    fix

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * free_grid - function that frees a 2 dimensional
+ * @gird: pinter to grid 2d array
+ * @height: ad array
+ * Return: void
+ */
 void free_grid(int **grid, int height)
 {
 	int i = 0;
