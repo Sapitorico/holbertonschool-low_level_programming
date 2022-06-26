e3d6957d6f5797416c31b46ad2a03d41b5380824
Sapitorico <renzoyaques@gmail.com>
2022-06-25 22:35:32 -0700

    fix

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,9 +11,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i = 0;
 	char *marray;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-	marray = malloc(nmemb * size);
+	marray = malloc(sizeof(char) * nmemb * size);
 		if (marray == 0)
 			return (NULL);
 	while (i < nmemb)
