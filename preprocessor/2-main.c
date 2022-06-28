f5fdeaf01a9314a948f60b294b13da51e735a74e
Sapitorico <renzoyaques@gmail.com>
2022-06-28 07:13:10 -0700

    prints the name of the file it was compiled from

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the name fale it was compiled
+ * Return: the name file
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return(0);
+}
