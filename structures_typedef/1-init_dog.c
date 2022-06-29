71ccd281919156d6ae666f00abecaf3c22187ec6
Sapitorico <renzoyaques@gmail.com>
2022-06-29 07:15:11 -0700

    soy un banana

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * int_dog - initialize truct dog
+ * @d: name
+ * @name:struct name
+ * @age: struct age
+ * @owner: truct owner
+ * return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != 0)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,19 @@
-#ifdef DOG_H
+#ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a dog info
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
+/*prototpes*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif/*DOG_H*/
