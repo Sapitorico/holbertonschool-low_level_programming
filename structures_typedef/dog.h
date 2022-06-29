442415ecfc2ddf2cd204354b7ed7402a0771552d
Sapitorico <renzoyaques@gmail.com>
2022-06-29 07:21:43 -0700

    fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -12,8 +12,9 @@ struct dog
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t;
 
 /*prototpes*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
 #endif/*DOG_H*/
