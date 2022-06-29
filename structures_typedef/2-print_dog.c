2b18ff6b13a16eb7d123fa36dc3ee55808e9d974
Sapitorico <renzoyaques@gmail.com>
2022-06-29 08:13:42 -0700

    fix

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,7 +18,13 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		}
 		printf("Age: %f\n", d->age);
-
-		printf("Owner: %s\n", d->owner);
+		if(d->owner == 0)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
 	}
 }
