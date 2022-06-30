208ed052a85ee7b74be7ff3ad32ca8359dd75ae3
Sapitorico <renzoyaques@gmail.com>
2022-06-29 19:17:26 -0700

    fix output

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,13 +1,13 @@
 #include "function_pointers.h"
 /**
- * array_interator - funtion axecutes fucntion in array
+ * array_iterator - funtion axecutes fucntion in array
  * @array: array
  * @size: aize of array
  * @action: perfonre to array
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (action)
+	if (action && array)
 		while (size--)
 			action(*array++);
 }
