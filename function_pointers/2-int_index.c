dca135a1c1848c2bdc448f3c382d43c2d905ece5
Sapitorico <renzoyaques@gmail.com>
2022-06-29 19:32:34 -0700

    function an integer

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that searches of integer
+ * @array: pinter array
+ * @size: size array
+ * @cmp:use compare values
+ * Retrun: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return(i);
+	return (-1);
+}
