5bc6c2a03519b7116bec73e3c83ce6f30baeb934
Sapitorico <renzoyaques@gmail.com>
2022-06-29 19:15:06 -0700

    executes a function given as a parameter

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * array_interator - funtion axecutes fucntion in array
+ * @array: array
+ * @size: aize of array
+ * @action: perfonre to array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (action)
+		while (size--)
+			action(*array++);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef function_pointer_h
 #define function_pointer_h
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif/*function_pinter_h*/
