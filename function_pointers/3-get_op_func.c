9c24346b2e4f507050728f1b2942042171b1b49d
Sapitorico <renzoyaques@gmail.com>
2022-06-30 17:57:27 -0700

    wqd

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -8,14 +8,14 @@
 int (*get_op_func(char *s))(int, int)
 {
 /*operation function structure*/
-	op_t ops[] = {
-        		{"+", op_add},
-        		{"-", op_sub},
-        		{"*", op_mul},
-        		{"/", op_div},
-        		{"%", op_mod},
-        		{NULL, NULL}
-		};
+op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
 
  	int i = 0;
 
