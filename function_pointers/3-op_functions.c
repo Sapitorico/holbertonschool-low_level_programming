859e2f0f69a3fa70c7c8a02a2a5648b784830607
Sapitorico <renzoyaques@gmail.com>
2022-06-30 17:49:49 -0700

    fix betty

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -2,11 +2,11 @@
 /**
  * op_add - functio
  * @a: first operand
- * @b:
+ * @b: secon operand
  */
 int op_add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
 /**
  * op_sub - function
@@ -15,7 +15,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	return(a - b);
+	return (a - b);
 }
 /**
  * op_mul - function
@@ -24,7 +24,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
 /**
  * op_div - function
@@ -33,7 +33,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	return(a / b);
+	return (a / b);
 }
 /**
  * op_mod -fucntion
@@ -42,5 +42,5 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	return(a % b);
+	return (a % b);
 }
