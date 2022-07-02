adbe9774742e609e23ec16842cf7bc1727caf171
Sapitorico <renzoyaques@gmail.com>
2022-07-02 10:56:03 -0700

    wqd

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -87,7 +87,7 @@ void print_all(const char * const format, ...)
 			if (!strcmp(format, funcs_ls[const2].types))
 			{
 				funcs_ls[const2].f(args);
-				printf("%s", *separator);
+				printf("%s", *separator != '\0');
 			}
 			const2++;
 		}
