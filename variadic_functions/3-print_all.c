b21a8e196549be6cf60f7b284ada43eedd59fb32
Sapitorico <renzoyaques@gmail.com>
2022-07-02 11:40:03 -0700

    we

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -80,10 +80,10 @@ void print_all(const char * const format, ...)
 	while (format && format[const1])
 	{
 		const2 = 0;
-		while (const2 < 4 && funcs_ls[const2].types)
+		while (funcs_ls[const2].types)
 		{
 			/*compare strings character by character*/
-			if (!strcmp(format, funcs_ls[const2].types))
+			if (const2 < 4 && !strcmp(format, funcs_ls[const2].types))
 			{
 				funcs_ls[const2].f(args);
 				printf("%s", *separator);
