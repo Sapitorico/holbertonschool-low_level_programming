40e04eee0d9107e835efc3a5cf77ff09160fafea
Sapitorico <renzoyaques@gmail.com>
2022-07-13 20:44:50 -0700

    0task

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: lists pinter
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		printf("[%d]%s\n ", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
