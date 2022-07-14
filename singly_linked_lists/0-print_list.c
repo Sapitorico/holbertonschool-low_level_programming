b2ae5d2fd70dd124d874ab33110e9f337bfc9870
Sapitorico <renzoyaques@gmail.com>
2022-07-13 20:55:25 -0700

    0task

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,9 +10,10 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		printf("[%d] %s\n", h->len, h->str);
+		if (!h->str)
+			printf("[0] (nil)\n");i
+		else		
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
