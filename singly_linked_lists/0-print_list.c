73374145cca877276d038a89a0fff4f6d0ddd0f6
Sapitorico <renzoyaques@gmail.com>
2022-07-13 20:48:56 -0700

    0task

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
-		printf("[%d]%s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
