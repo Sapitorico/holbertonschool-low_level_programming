fce5e4cf7ae2ca8bcd87aae4357f59f7016f35c6
Sapitorico <renzoyaques@gmail.com>
2023-02-02 14:41:41 -0300

    fix error: value computed is not used

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,7 +12,8 @@ int main(void)
     while (number != 58)
     {
         write(1, &number, 1), number++;
-        (number < 58) && write(1, &separator1, 1), write(1, &separator2, 1);
+        if (number < 58) write(1, &separator1, 1),\
+        write(1, &separator2, 1);
     }
     putchar('\n');
     return (0);
