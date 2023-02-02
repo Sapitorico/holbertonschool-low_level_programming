4f56d1f777eb96063864a44a2dc2d2ad4f2e382c
Sapitorico <renzoyaques@gmail.com>
2023-02-02 09:57:42 -0300

    function that prints out which single is a randomly assigned number

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - this program assigned a random number n
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	(n > 0) ? printf("%d is positive\n", n) : \
+	(n < 0) ? printf("%d is negative\n", n) : printf("%d is zero\n", n);
+	return (0);
+}
