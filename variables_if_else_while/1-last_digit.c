9c7f81d92fb24051e064dd75974b76971db5d1f2
Sapitorico <renzoyaques@gmail.com>
2023-02-02 11:03:56 -0300

    checks the last digit of a number with conditionals

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - assigned a random number in variable n
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+    int last_number = n % 10;
+
+    printf("Last digit of %d is %d ", n, last_number);
+    (last_number > 5) ? printf("and is greater than 5\n") : \
+    (last_number < 6 && n != 0) ? printf("and is less than 6 and not 0\n") : \
+    printf("and is 0\n");
+    return (0);
+}
