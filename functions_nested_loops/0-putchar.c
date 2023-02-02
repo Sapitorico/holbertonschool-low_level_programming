c36402707285ebd611389922a904c51e212fdbb1
Sapitorico <renzoyaques@gmail.com>
2023-02-02 14:56:12 -0300

    print puthcar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - print usage write
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+    char *putchar = "_putchar\n";
+
+    write(1, putchar, 9);
+    return (0);
+}
