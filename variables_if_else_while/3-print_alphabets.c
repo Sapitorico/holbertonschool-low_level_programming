a1d67a860889c1d0f2f014bdc687e6c0813a2263
Sapitorico <renzoyaques@gmail.com>
2023-02-02 14:03:12 -0300

    fix print z in lower case

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@ int main(void)
     while (alphabet != 91)
     {
         write(1, &alphabet, 1), alphabet++;
-        if (alphabet == 122) alphabet = 65;
+        if (alphabet == 123) alphabet = 65;
     }
     putchar('\n');
     return (0);
