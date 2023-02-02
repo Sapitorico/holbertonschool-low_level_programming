48b144717668e963eb67bcfc7d8ae7f87aa19172
Sapitorico <renzoyaques@gmail.com>
2023-02-02 12:47:45 -0300

    fix condition that last number is 0

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,12 +11,13 @@ int main(void)
 	int n;
     int last_number = 0;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	// srand(time(0));
+	// n = rand() - RAND_MAX / 2;
+    n = 980;
     last_number = n % 10;
     printf("Last digit of %d is %d ", n, last_number);
     (last_number > 5) ? printf("and is greater than 5\n") : \
-    (last_number < 6 && n != 0) ? printf("and is less than 6 and not 0\n") : \
+    (last_number < 6 && last_number != 0) ? printf("and is less than 6 and not 0\n") : \
     printf("and is 0\n");
     return (0);
 }
