4f9ed506dd1a4f367d4e784d5a603c947540d8de
Sapitorico <renzoyaques@gmail.com>
2023-02-02 13:59:42 -0300

    remove a variabel

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,7 +13,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    n = 980;
     last_number = n % 10;
     printf("Last digit of %d is %d ", n, last_number);
     (last_number > 5) ? printf("and is greater than 5\n") : \
