9b150a795cf937b8b67a844e2311911dbb34b7ae
Sapitorico <renzoyaques@gmail.com>
2023-02-02 14:00:50 -0300

    print alphabet in lowercase and uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - function that print alphabet in lower case and upper case
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+    char alphabet = 97;
+
+    while (alphabet != 91)
+    {
+        write(1, &alphabet, 1), alphabet++;
+        if (alphabet == 122) alphabet = 65;
+    }
+    putchar('\n');
+    return (0);
+}
