0789cc758f184e24a94525d5fa5df9871935eaf5
Sapitorico <renzoyaques@gmail.com>
2023-02-03 08:41:39 -0300

    check a character is lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - checke for lowercase a character
+ *
+ * @c: a character
+ * Return: 1 is a lower, 0 is not
+ */
+int _islower(int c)
+{
+    (c >= 97 && c <= 122) ? c = 1 : c = 0;
+    return (c);
+}
