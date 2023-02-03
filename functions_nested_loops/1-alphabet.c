7a624ba387367b3117d148045c7ceca2ce5acc70
Sapitorico <renzoyaques@gmail.com>
2023-02-02 23:21:45 -0300

    prints 10 times the alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - print alphabet
+ * print_alphabet - print alphabet
  *
  * Return: always 0
  */
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "1-alphabet.c"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+    int i = 0;
+
+    for (i = 0; i != 10; i++)
+    {
+        print_alphabet();
+    }
+}
