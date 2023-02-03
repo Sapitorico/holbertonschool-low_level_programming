4e4176133d0487a9de6d3da5b18fe1f12efb5e36
Sapitorico <renzoyaques@gmail.com>
2023-02-03 12:35:34 -0300

    print hours and minutes

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ */
+void jack_bauer(void)
+{
+    int hours = 0, minutes = 0;
+
+    for (hours = 0; hours < 24; hours++)
+    {
+        for (minutes = 0; minutes < 60; minutes ++)
+        {
+            _putchar(hours / 10 + '0');
+            _putchar(hours % 10 + '0');
+            write(1, ":", 1);
+            _putchar(minutes / 10 + '0');
+            _putchar(minutes % 10 + '0');
+            write(1, "\n", 1);
+        }
+    }
+}
