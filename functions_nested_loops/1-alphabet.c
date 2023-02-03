8161edbb07fd5e83a34f0ee7c37910f95e599c87
Sapitorico <renzoyaques@gmail.com>
2023-02-02 23:06:43 -0300

    define cundtion that print alphhabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+    char alphabet = 97;
+
+    while (alphabet != 123)
+    {
+        write(1, &alphabet, 1), alphabet++;
+    }
+    putchar('\n');
+}
