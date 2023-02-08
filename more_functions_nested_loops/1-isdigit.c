602e76f32ec88d611db400e2f8df7bb548350496
Sapitorico <renzoyaques@gmail.com>
2023-02-08 15:17:40 -0300

    chek is a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ *
+ * @c: digit
+ * Return: 1 is digit 0 or not
+ */
+int _isdigit(int c)
+{
+    return (c >= '0' && c <= '9');
+}
