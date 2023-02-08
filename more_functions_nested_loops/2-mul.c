b5d7e72e578fb63bfc6856ff243c6e995c62d397
Sapitorico <renzoyaques@gmail.com>
2023-02-08 15:18:01 -0300

    mutipplicate a two inters

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two intergers
+ *
+ * @a: integer1
+ * @b: integer2
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
