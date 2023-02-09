38125d2372ef205ca266c9fc17084011b1488877
Sapitorico <renzoyaques@gmail.com>
2023-02-09 15:09:43 -0300

    copie a string

---
+++ pointers_arrays_strings/C_Pointers_arrays_and_string/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copies to string
+ *
+ * @dest: buffer pointed dest
+ * @src: string pointed
+ * Return: pointer to new copy stirng
+ */
+char *_strcpy(char *dest, char *src)
+{
+    char *copy = dest;
+
+
+    while (*copy++ = *src++)
+        ;
+    return (dest);
+}
