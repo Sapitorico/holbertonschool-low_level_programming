a8d24e0a4c535ea320a9cd36e8ec8325e96d673f
Sapitorico <renzoyaques@gmail.com>
2023-02-11 07:28:54 -0300

    primt a sum of diagonal of matrix

---
+++ pointers_arrays_strings/C_Even_more_pointers_arrays_and_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_diagsums - print a sum of the two diagonals of matrix
+ *
+ * @a: matrix
+ * @size: size of matrix
+ */
+void print_diagsums(int *a, int size)
+{
+    int iterator = 0,
+    result1 = 0, result2 = 0;
+
+    for (iterator = 0; iterator < size; iterator++)
+        result1 += a[iterator * size + iterator],
+        result2 += a[size * (iterator + 1) - (iterator + 1)];
+    printf("%d, %d\n", abs(result1), abs(result2));
+}
